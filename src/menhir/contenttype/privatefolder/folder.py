ede1e6d760026135389f2cb5ed784a772f2b1259
Souheil CHELFOUH <trollfot@gmail.com>
2011-02-22 23:55:58 +0100

    Based on folder

--- src/menhir/contenttype/privatefolder/folder.py
+++ src/menhir/contenttype/privatefolder/folder.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import dolmen.content
-from dolmen.app.content import icon, IDescriptiveSchema
-from menhir.contenttype.folder import MCFMessageFactory as _
+from dolmen.app.content import icon
+from menhir.contenttype.folder import Folder, IFolder, MCFMessageFactory as _
 
 
-class IPrivateFolder(IDescriptiveSchema):
+class IPrivateFolder(IFolder):
     """Schema interface for private folders.
     """
 
 
-class PrivateFolder(dolmen.content.OrderedContainer):
+class PrivateFolder(Folder):
     icon('folder.png')
     dolmen.content.schema(IPrivateFolder)
     dolmen.content.name(_(u"Private folder"))
-    selected_view = "folderlisting"
